22104b4f3d1533a492023a4b8a71f8fcc009d7c2
刘宇 <n0378@qq.com>
2023-02-28 14:27:58 +0800

    update

---
+++ ML基础/TD_IDF.py
@@ -0,0 +1,75 @@
+import math
+from collections import Counter, defaultdict
+
+
+def compute_tf(text):
+    """
+    计算文本的词频
+    :param text: 文本字符串
+    :return: 词频字典
+    """
+    tf_dict = Counter(text.split())
+    for word in tf_dict:
+        tf_dict[word] = tf_dict[word] / len(text.split())
+    return tf_dict
+
+
+def compute_idf(docs):
+    """
+    计算逆文档频率
+    :param docs: 文档列表
+    :return: 逆文档频率字典
+    """
+    N = len(docs)
+    idf_dict = defaultdict(float)
+    for doc in docs:
+        for word, count in doc.items():  # 文档 1 的  A : 0.37
+            if count > 0:  # 如果 等于 0  就表示没出现
+                idf_dict[word] += 1
+    for word, count in idf_dict.items():
+        idf_dict[word] = math.log(N / count)
+    return idf_dict
+
+
+def compute_tfidf(tf, idf):
+    """
+    计算TF-IDF值
+    :param tf: 词频字典
+    :param idf: 逆文档频率字典
+    :return: TF-IDF字典
+    """
+    tfidf_dict = {}
+    for word, tf_value in tf.items():
+        tfidf_dict[word] = tf_value * idf[word]
+    return tfidf_dict
+
+
+def text_tfidf(docs):
+    tf_list = []
+    for doc in docs:
+        tf_list.append(compute_tf(doc))
+    idf = compute_idf(tf_list)
+
+    tfidf_list = []
+    for tf in tf_list:
+        tfidf_list.append(compute_tfidf(tf, idf))
+    return tfidf_list
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+if __name__ == '__main__':
+
+    # 示例
+    docs = [
+        "i love you",
+        "i love you to",
+        "you love me"
+    ]
+    tfidf_list = text_tfidf(docs)
+
+    tfidf = TfidfVectorizer()
+    X = tfidf.fit_transform(docs)
+
+    for i in range(len(docs)):
+        print(f"TF-IDF vector for sentence {i + 1}:\n {tfidf_list[i]}")
--- 机器学习/TF_IDF向量化.py
+++ 机器学习/TF_IDF向量化.py
@@ -1,8 +1,5 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import jieba
-from transformers import BertTokenizer
-
-t = BertTokenizer.from_pretrained("bert-base-chinese")
 
 # 示例文本
 text = [
@@ -11,10 +8,20 @@ text = [
     # doc 2
     "NLG 技术具有极为广泛的应用价值，应用于智能问答对话系统和机器翻译系统时，可实现更为智能便捷的人机交互"]
 text = [" ".join(jieba.cut(t)) for t in text]
-stop_words = ["。", "、"]
 # 创建TfidfVectorizer对象
-vectorizer = TfidfVectorizer(stop_words=stop_words)
+vectorizer = TfidfVectorizer()
 
 # 使用TfidfVectorizer进行文本特征提取
-X = vectorizer.fit_transform(text)
-print(X.toarray()[0])
+X = vectorizer.fit(text)
+# 输出关键词及其权重
+feature_names = vectorizer.get_feature_names_out()
+print(feature_names)
+for i, doc in enumerate(text):
+    print("Document ", i + 1)
+    tfidf_result = vectorizer.transform([doc])
+    print(doc)
+    print(tfidf_result)
+    assert 0
+    for j in tfidf_result.indices:
+        print(feature_names[j], tfidf_result[0, j])
+    print("\n")
