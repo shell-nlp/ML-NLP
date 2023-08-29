ca9e8b0ab7e9b9f9cc25f09efc2585ff140c5c72
刘宇 <n0378@qq.com>
2023-08-29 20:07:55 +0800

    rm some file

--- 项目实战/OpenPrompt/生成.py
+++
@@ -1,2 +0,0 @@
-from datasets import load_dataset
-# 参考url: https://blog.csdn.net/Can__er/article/details/127365337
--- 项目实战/国网抽取/main.py
+++
@@ -1,9 +0,0 @@
-from paddlenlp import Taskflow
-from pprint import pprint
-
-parens_path = './项目实战/国网抽取/'
-docprompt = Taskflow("document_intelligence", model='docprompt')
-pprint(docprompt([{"doc": parens_path+"data/2.png", "prompt": ["日期是多少?"]}]))
-pprint(docprompt([{"doc": parens_path+"data/1.png", "prompt": ["额定频率是多少?", "额定电压是多少?",
-       "样品型号是什么?", "制造单位是什么?", "制造单位地址在哪里?", "出厂编号是多少?", "委托编号是多少?"]}]))
-pprint(docprompt([{"doc": parens_path+"data/3.png", "prompt": ["样品名称是多少?"]}]))
